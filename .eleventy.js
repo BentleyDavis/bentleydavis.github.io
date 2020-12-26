2f8aedc74d2a283160601abfdfc51acfc170ea5c
Bentley Davis <Bentley@BentleyDavis.com>
2020-12-26 16:49:18 -0600

    removed fileImport in favor of nunjucks include

--- .eleventy.js
+++ .eleventy.js
@@ -2,17 +2,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "static-root": "/" });
 
-  eleventyConfig.addShortcode("fileImport", function (fileName) {
-    const fs = require('fs');
-    const path = require('path');
-    const dirPath = process.cwd();
-    const filePath = path.join(dirPath,"_includes","shortcodes", fileName);
-    if (fs.existsSync(filePath)) {
-      var buffer = fs.readFileSync(filePath).toString();
-    }
-    return buffer;
-  });
-
   eleventyConfig.addShortcode("formatDate", function(date) { 
     return "" + date.toLocaleDateString(
       "en-US",
