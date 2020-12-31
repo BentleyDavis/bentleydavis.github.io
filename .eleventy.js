1521e84f5d1c5e02bc8b6a0c11d4f14ab9c420a3
Bentley Davis <Bentley@BentleyDavis.com>
2020-12-31 10:05:29 -0600

    copy imaes in same folder as source page

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,33 @@
+const fs = require("fs");
+const Path = require("path");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "static-root": "/" });
 
-  eleventyConfig.addShortcode("formatDate", function(date) { 
+  eleventyConfig.addShortcode("formatDate", function (date) {
     return "" + date.toLocaleDateString(
       "en-US",
       { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
     ) + "";
+  });
+
+  eleventyConfig.addShortcode("copyImages", function (page, img) {
+    const files = fs.readdirSync(Path.dirname(page.inputPath))
+    for (file of files) {
+      if (
+        file.endsWith(".png") ||
+        file.endsWith(".jpeg") ||
+        file.endsWith(".jpg") ||
+        file.endsWith(".gif")
+        ) {
+        fs.mkdirSync(Path.dirname(page.outputPath), { recursive: true });
+        fs.copyFileSync(
+          Path.join(Path.dirname(page.inputPath), file),
+          Path.join(Path.dirname(page.outputPath), file)
+        )
+      }
+    }
+    return ""
   });
 
   return {
