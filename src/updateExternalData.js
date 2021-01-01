26c235fbdd85ff493c5caf4140da9e4fe818b1db
Bentley Davis <Bentley@BentleyDavis.com>
2021-01-01 17:27:45 -0600

    started recommendations

--- src/updateExternalData.js
+++ src/updateExternalData.js
@@ -3,16 +3,23 @@ const Path = require("path");
 const getUrlMetadata = require('../src/getUrlMetadata.js');
 
 (async () => {
+    // recomendations = JSON.parse(await fs.promises.readFile('_data/recomendations.json', 'utf8'));
+    // await processUrls(recomendations, "linkedIn")
+    //  //Linked in is blocking requests
+
     skills = JSON.parse(await fs.promises.readFile('_data/skills.json', 'utf8'));
-    for (skill of skills) {
-        if (skill.autoUpdate) {
+    await processUrls(skills)
+})();
+
+async function processUrls(items, urlProperty = "url") {
+    for (item of items) {
+        if (item.autoUpdate) {
             await getUrlMetadata.getUrlMetadata(
-                skill.url,
-                `_data/external/${skill.id}`,
-                `img/external/${skill.id}`
+                item[urlProperty],
+                `_data/external/${item.id}`,
+                `img/external/${item.id}`
             );
         }
     }
-
-})();
+}
 
