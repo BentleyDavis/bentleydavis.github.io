61243f4e257bb4075cca9de3b08e5c41f60de029
Bentley Davis <Bentley@BentleyDavis.com>
2021-01-12 15:34:32 -0600

    added CMS thumbnails

--- content/portfolio/portfolio.11tydata.js
+++ content/portfolio/portfolio.11tydata.js
@@ -1,10 +1,13 @@
 module.exports = {
-    eleventyComputed: {
-        title: data => {
-            return data.item.title;
-          },
-        tags: (data) => {
-            return data.item.Tags;
-          }
+  eleventyComputed: {
+    title: data => {
+      return data.item.title;
+    },
+    tags: (data) => {
+      return data.item.Tags;
+    },
+    img: (data) => {
+      return data.page.url + data.item.thumbnail;
     }
-  };
+  }
+};
