d42a258ac6ceadae993ec6144c3f612633815596
Bentley Davis <Bentley@BentleyDavis.com>
2021-01-20 12:57:08 -0600

    add skill amp

--- src/updateExternalData.js
+++ src/updateExternalData.js
@@ -1,19 +1,21 @@
 const fs = require("fs").promises;
-const getAirtable = require('./getDataAirTable');
+//const getAirtable = require('./getDataAirTable');
 const { processUrls } = require("./processUrls");
-const downloadAirtableImages = require("./downloadAirtableImages");
+//const downloadAirtableImages = require("./downloadAirtableImages");
 
 (async () => {
 
     // Skills
-    const skills = await getAirtable('skills');
+    //const skills = await getAirtable('skills');
+    const skills = JSON.parse(await fs.readFile(`./_data/skills.json`,'utf8'));
+
     await processUrls(skills)
-    await fs.writeFile(`./_data/skills.json`, JSON.stringify(skills));
+    //await fs.writeFile(`./_data/skills.json`, JSON.stringify(skills));
 
     // Recommendations
-    const recommendations = await getAirtable('recommendations');
-    await downloadAirtableImages(recommendations,'recommendations')
-    await fs.writeFile(`./_data/recommendations.json`, JSON.stringify(recommendations));
+    //const recommendations = await getAirtable('recommendations');
+    // await downloadAirtableImages(recommendations,'recommendations')
+    // await fs.writeFile(`./_data/recommendations.json`, JSON.stringify(recommendations));
     
 })();
 
