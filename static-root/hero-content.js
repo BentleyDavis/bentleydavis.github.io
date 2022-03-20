95ff2701459b45a70e72dc52dec4ccfea812beae
Bentley Davis <Bentley@BentleyDavis.com>
2022-03-19 19:37:02 -0500

    less capitalistic

--- static-root/hero-content.js
+++ static-root/hero-content.js
@@ -1,23 +1,17 @@
 const typeText = [
-    { id: "ht-01", text: "I increase your" },
-    { id: "ht-02", text: "profit" },
-    { id: "ht-03", text: "and" },
-    { id: "ht-04", text: "time" },
-    { id: "ht-05", text: "through technology" },
-    { id: "ht-06", text: "and training." },
-    { id: "ht-07", text: "- Interactive Websites" },
-    { id: "ht-08", text: "- Mobile Apps" },
-    { id: "ht-09", text: "- Automation" },
-    { id: "ht-10", text: "- Fractional CTO" },
+    { id: "ht-01", text: "more satisfaction for" },
+    { id: "ht-015", text: "more people though technology" },
+    { id: "ht-02", text: "think visually to work brilliantly" },
+    { id: "ht-03", text: "solve big problems with small experiments" },
+    { id: "ht-04", text: "interactive websites, mobile apps" },
+    { id: "ht-05", text: "training you to use technology" },
+    { id: "ht-06", text: "automating your boring work" },
+    { id: "ht-07", text: "30 years creating software" },
+    { id: "ht-08", text: "20 years managng teams" },
+    { id: "ht-09", text: "5 years mentoring startups" },
+    { id: "ht-10", text: "CTO of a successful startup exit" },
 ]
-// const e = document.getElementById('bd-hero-dynamic');
 
-// e.innerHTML = `
-// <span id="ht-1"></span>
-// <span id="ht-2" style="font-size: 200%;"></span>
-// <span id="ht-3"></span>
-// <span id="ht-4" style="font-size: 200%;"></span>
-// `
 let sectionCounter = 0;
 let characterCounter = 0;
 let currentElement = document.getElementById(typeText[sectionCounter].id)
